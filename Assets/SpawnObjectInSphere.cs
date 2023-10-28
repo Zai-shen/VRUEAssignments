9a9dbcd6b1834bf759bc30a5bcfc9ff1db18c703
Miran Jank <ma0069@technikum-wien.at>
2023-10-29 00:10:23 +0200

    Added Cue Ball spawning, Made Cue grabable

---
+++ Assets/SpawnObjectInSphere.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpawnObjectInSphere : MonoBehaviour
+{
+    public GameObject ObjectPrefab;
+    public float SphereSize = 0.5f;
+
+    private GameObject _spawnedGO;
+    
+    private void Start()
+    {
+        _spawnedGO = Instantiate(ObjectPrefab, transform);
+        SetRandomPosition();
+    }
+
+    [ContextMenu("SetRandomPosition")]
+    public void SetRandomPosition()
+    {
+        _spawnedGO.transform.position = transform.position + RandomPointOnSphere(SphereSize);
+    }
+    
+    private Vector3 RandomPointOnSphere(float radius)
+    {
+        return Random.insideUnitSphere.normalized * radius;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, SphereSize);
+    }
+}
